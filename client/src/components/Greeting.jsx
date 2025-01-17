c8b29aecdc8501303d069d605289c29d04492435
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-01-17 18:53:21 +0530

    Configured Docker Container to k8 merge crowzerks

---
+++ client/src/components/Greeting.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function Greeting() {
+    const [greeting, setGreeting] = useState(null);
+
+    useEffect(() => {
+        fetch('/api/greeting')
+            .then((res) => res.json())
+            .then((data) => setGreeting(data.greeting));
+    }, [setGreeting]);
+
+    if (!greeting) return null;
+
+    return <div>
+            <h1 className="text-center mb-5">{greeting}</h1>;
+        </div>
+}
